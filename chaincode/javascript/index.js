42eb4223d592db8c947cf289eab274219024f637
Skanda S <71484498+Skanda19cs137@users.noreply.github.com>
2023-04-16 21:45:23 +0530

    chaincode

---
+++ chaincode/javascript/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const PCS = require('./lib/PCS');
+
+module.exports.PCS = PCS;
+module.exports.contracts = [ PCS ];
